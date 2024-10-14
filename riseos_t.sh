79f0bb3e495129993117309f9e871c87f5c0fb90
Demi <kizura.vian@gmail.com>
2024-10-14 19:31:05 +0800

    Update riseos_t.sh

--- riseos_t.sh
+++ riseos_t.sh
@@ -39,6 +39,10 @@ rm -rf hardware/qcom-caf/sm8150/media
 rm -rf vendor/lineage
 rm -rf packages/apps/Updater
 rm -rf frameworks/base
+
+# Setup Environment (LineageOS)
+sudo apt-get update -y && sudo apt-get install dialog bash sed wget git curl zip tar jq expect make cmake automake autoconf llvm lld lldb clang gcc binutils bison perl gperf gawk flex bc python3 zstd openssl unzip cpio bc bison build-essential ccache liblz4-tool libsdl1.2-dev libstdc++6 libxml2 libxml2-utils lzop pngcrush schedtool squashfs-tools xsltproc zlib1g-dev libncurses5-dev bzip2 git gcc g++ libssl-dev gcc-aarch64-linux-gnu gcc-arm-linux-gnueabihf gcc-arm-linux-gnueabi dos2unix kmod python2 tree -y
+
 # cloning DT
 # device tree
 git clone https://github.com/alonsoj636MW/device_xiaomi_sunny.git --depth 1 -b 14 device/xiaomi/sunny

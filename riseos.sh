e995372b66011acd5255ca7ee489882168fd3564
Deependra Shrestha <36838592+dpenra@users.noreply.github.com>
2024-10-16 17:53:18 +0545

    Update riseos.sh

--- riseos.sh
+++ riseos.sh
@@ -46,7 +46,7 @@ git clone https://github.com/dpenra-sunny/device_xiaomi_sunny-kernel.git --depth
 git clone https://github.com/AOSPA/android_device_qcom_qssi.git --depth 1 -b uvite device/qcom/qssi
 git clone https://github.com/yaap/device_qcom_common.git --depth 1 -b fourteen device/qcom/common
 # kernel tree
-git clone https://github.com/dpenra-sunny/kernel_xiaomi_sunny.git --depth 1 kernel/xiaomi/sunny
+# git clone https://github.com/dpenra-sunny/kernel_xiaomi_sunny.git --depth 1 kernel/xiaomi/sunny
 # vendor tree
 git clone https://github.com/dpenra-sunny/vendor_xiaomi_sunny.git --depth 1 -b fourteen vendor/xiaomi/sunny
 git clone https://gitlab.com/yaosp/vendor_qcom_common.git --depth 1 -b fourteen vendor/qcom/common
